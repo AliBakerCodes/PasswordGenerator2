685ef50d622e91dbba61574f823785c55b824349
AliBaker <alibakerconsulting@gmailcom>
2022-03-17 18:32:47 -0400

    started adding controls and styling

--- script.js
+++ script.js
@@ -140,3 +140,12 @@ function getUserInput() {
     passwordInpt.special = true;
   }
 }
+
+var slider = document.getElementById("myRange");
+var output = document.getElementById("demo");
+output.innerHTML = slider.value; // Display the default slider value
+
+// Update the current slider value (each time you drag the slider handle)
+slider.oninput = function() {
+  output.innerHTML = this.value;
+}
